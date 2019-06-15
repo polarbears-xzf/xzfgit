df4f8819e6310a037ae8b54ddb7e4736a97f4d31
yangzheng <526897654@qq.com>
2019-06-15 11:45:04 +0800

    !8 巡检告警补充 Merge pull request !8 from yangzheng/master

--- zoeMonitor/checkup_report/alert_db_space.sql
+++ zoeMonitor/checkup_report/alert_db_space.sql
@@ -22,10 +22,36 @@ set markup html off
 prompt <br />
 
 DECLARE
-	lv_is_rac  VARCHAR2(3);
-	lv_is_archive VARCHAR2(3);
+--	lv_is_rac  VARCHAR2(3);
+--	lv_is_archive VARCHAR2(3);
+CURSOR tab_cursor IS
+-- 查询使用率大于70% 同时剩余表空间小于100Gb的表空间
+select tablespace_name,pct_used from
+(
+	select tablespace_name,
+       (max_gb-used_gb) free_gb,
+       round(100 * used_gb / max_gb) pct_used
+	from (select a.tablespace_name tablespace_name,
+               round((a.bytes_alloc - nvl(b.bytes_free, 0)) / power(2, 30),
+                     2) used_gb,
+               round(a.maxbytes / power(2, 30), 2) max_gb
+          from (select f.tablespace_name,
+                       sum(f.bytes) bytes_alloc,
+                       sum(decode(f.autoextensible,
+                                  'YES',
+                                  f.maxbytes,
+                                  'NO',
+                                  f.bytes)) maxbytes
+                  from dba_data_files f
+                 group by tablespace_name) a,
+               (select f.tablespace_name, sum(f.bytes) bytes_free
+                  from dba_free_space f
+                 group by tablespace_name) b
+         where a.tablespace_name = b.tablespace_name(+)))
+where pct_used>70 and  free_gb <100;
+
 BEGIN
-	--是否RAC
+/* 	--是否RAC
 	select decode(value,'TRUE','是','FALSE','否',value)  INTO lv_is_rac
 	from gv$parameter where name = 'cluster_database';
 	IF lv_is_rac = '否' THEN 
@@ -38,7 +64,20 @@ BEGIN
 		DBMS_OUTPUT.PUT_LINE('<table WIDTH=600 BORDER=1>');
 		DBMS_OUTPUT.PUT_LINE(' <td> 紧急：数据库未开启归档模式，存在严重安全隐患</td> ');
 		DBMS_OUTPUT.PUT_LINE('</table> ');
-	END IF;
+	END IF; */
+	
+	FOR t in tab_cursor loop
+		if t.pct_used > 80 and t.pct_used <90 then
+			DBMS_OUTPUT.PUT_LINE('<table WIDTH=600 BORDER=1>');
+			DBMS_OUTPUT.PUT_LINE('<td>表空间'||t.tablespace_name||'余量较少,请在一周内扩展');
+			DBMS_OUTPUT.PUT_LINE('</table> ');
+		end if;
+		if t.pct_used > 90  then
+			DBMS_OUTPUT.PUT_LINE('<table WIDTH=600 BORDER=1>');
+			DBMS_OUTPUT.PUT_LINE('<td>表空间'||t.tablespace_name||'余量极少,请立即扩展');
+			DBMS_OUTPUT.PUT_LINE('</table> ');
+		end if;
+	end loop;
 	
 END;
 /
