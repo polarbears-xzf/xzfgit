abcb431c5664d8211355691e737fd1eefb9fb38c
me <526897654@qq.com>
2019-06-19 16:46:43 +0800

    1.增加当数据文件数量接近db_files参数时的告警 2.增加控制文件数量的告警

--- zoeMonitor/checkup_report/alert_db_physic.sql
+++ zoeMonitor/checkup_report/alert_db_physic.sql
@@ -17,27 +17,54 @@
 	--控制文件数：小于2时进行提示处理
 	
 set markup html off
-prompt <br />
+--prompt <br />
 
-DECLARE
+/* DECLARE
 	lv_is_rac  VARCHAR2(3);
-	lv_is_archive VARCHAR2(3);
+	lv_is_archive VARCHAR2(3); */
+DECLARE
+    DB_FILE_DIFF number(5);
+    CON_FILES number(5);
+	   
+	   
 BEGIN
-	--是否RAC
-	select decode(value,'TRUE','是','FALSE','否',value)  INTO lv_is_rac
-	from gv$parameter where name = 'cluster_database';
-	IF lv_is_rac = '否' THEN 
-		DBMS_OUTPUT.PUT_LINE('Oracle RAC 未部署，系统存在高可用性缺陷');
-	END IF;
-	--是否开启归档
-	select decode(log_mode,'ARCHIVELOG','是','否')       INTO lv_is_archive
-	from v$database;
-	IF lv_is_archive = '否' THEN 
+
+-- 查询数据文件剩余可用量；
+select (max_db_files - db_files) difference into DB_FILE_DIFF
+   from (select value max_db_files from v$parameter where NAME = 'db_files'),
+        (select count(*) db_files from v$datafile);
+		 
+-- 查询控制文件数量
+select type into CON_FILES from v$parameter where NAME = 'control_files';
+
+--判断数据文件剩余可用量值范围   
+	if DB_FILE_DIFF >= 10 and  DB_FILE_DIFF < 30 then
 		DBMS_OUTPUT.PUT_LINE('<table WIDTH=600 BORDER=1>');
-		DBMS_OUTPUT.PUT_LINE(' <td> 紧急：数据库未开启归档模式，存在严重安全隐患</td> ');
+		DBMS_OUTPUT.PUT_LINE('<td> 注意:最大数据文件剩余可用数量='|| DB_FILE_DIFF ||',少于30个,请注意添加');
 		DBMS_OUTPUT.PUT_LINE('</table> ');
-	END IF;
-	
+	else if  DB_FILE_DIFF < 10 and DB_FILE_DIFF >= 0 then
+		DBMS_OUTPUT.PUT_LINE('<table WIDTH=600 BORDER=1>');
+		DBMS_OUTPUT.PUT_LINE('<td> 告警:最大数据文件剩余可用数量='|| DB_FILE_DIFF ||',少于10个,请立即添加');
+		DBMS_OUTPUT.PUT_LINE('</table> ');
+	else 
+		DBMS_OUTPUT.PUT_LINE('<table WIDTH=600 BORDER=1>');
+		DBMS_OUTPUT.PUT_LINE('<td> 数据文件剩余可用数量:'|| DB_FILE_DIFF);
+		DBMS_OUTPUT.PUT_LINE('</table> ');
+		end if;
+	end if;
+
+--判断控制文件数量
+	if con_files < 2 and con_files >=0 then
+		DBMS_OUTPUT.PUT_LINE('<table WIDTH=600 BORDER=1>');
+        DBMS_OUTPUT.PUT_LINE('<td> 控制文件数量少于2个,请立即增加控制文件');
+		DBMS_OUTPUT.PUT_LINE('</table> ');
+    else
+		DBMS_OUTPUT.PUT_LINE('<table WIDTH=600 BORDER=1>');
+        DBMS_OUTPUT.PUT_LINE('<td> 控制文件数量:'|| CON_FILES );
+		DBMS_OUTPUT.PUT_LINE('</table> ');
+    end if;
+
+			   
 END;
 /
 
