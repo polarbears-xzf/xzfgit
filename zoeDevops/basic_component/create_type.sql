8d1b2f6711597e17a9b9b9e1240f0051f58f4ac1
polarbears-xzf <43983324@qq.com>
2019-06-20 17:54:41 +0800

    重构运维管理基本功能组件

---
+++ zoeDevops/basic_component/create_type.sql
@@ -0,0 +1,24 @@
+-- Created in 2019.06.20 by polarbears
+-- Copyright (c) 20xx, CHINA and/or affiliates.
+-- All rights reserved.
+--	Name:
+-- 		create_type.sql
+--	Description:
+-- 		
+--  Relation:
+--      
+--	Notes:
+--		
+--	修改 - （年-月-日） - 描述
+
+--用于数据库基本信息，区分唯一数据库
+CREATE OR REPLACE TYPE ZOEDEVOPS.zoeto_db_basic_info AS OBJECT(
+	HOST_NAME     VARCHAR2(128),
+	IP_ADDRESS    VARCHAR2(128),
+	DB_NAME       VARCHAR2(128),
+	CREATED_TIME  DATE,
+	DB_VERSION    VARCHAR2(80)
+)
+/
+CREATE OR REPLACE TYPE ZOEDEVOPS.zoett_db_basic_info AS TABLE OF zoeto_db_basic_info;
+/
