e97fd215a3d9fbfd2e0713e3f44de3c2eb0bceaa
曾胖子 <19906058349@163.com>
2019-07-05 08:34:43 +0800

    !31 项目基本信息和操作系统基本信息调整以及增加 Merge pull request !31 from 曾胖子/master

--- zoeMonitor/checkup_report/content_os_basic_status.sql
+++ zoeMonitor/checkup_report/content_os_basic_status.sql
@@ -13,12 +13,13 @@
 --
 
 -- =======================================
--- 数据库基本状态信息
+-- 操作系统基本状态信息
 -- =======================================
 	--	CPU
 	--	内存
 	--	磁盘
 	
+/*
 set markup html off
 prompt  <H3 class='zoecomm'> <center> 操作基本状态信息 </center>  </H3>
 set markup html on entmap off
@@ -37,4 +38,49 @@ from v$osstat
 where STAT_NAME IN ('NUM_CPUS','','PHYSICAL_MEMORY_BYTES','FREE_MEMORY_BYTES','VM_IN_BYTES','VM_OUT_BYTES');
 
 prompt  </center>
+*/
+
+set markup html off
+
+prompt  <H3 class='zoecomm'>  <center> 操作系统基本状态信息 </center> </H3> <br>
+
+--操作系统活动cpu个数
+column NUM_CPUS  NEW_VALUE CPUS noprint
+select value "NUM_CPUS" from v$osstat where STAT_NAME='NUM_CPUS';
+--操作系统物理内存大小
+column PHYSICAL_MEMORY_BYTES NEW_VALUE MEMORYBYTES noprint
+select ''||trunc(value/1024/1024,2)||'M' as "PHYSICAL_MEMORY_BYTES" from v$osstat where STAT_NAME='PHYSICAL_MEMORY_BYTES';
+--定义VM_IN_BYTES
+column VM_IN_BYTES  NEW_VALUE VM_IN_BYTES noprint
+select to_char(value) "VM_IN_BYTES" from v$osstat where STAT_NAME='VM_IN_BYTES';
+--定义VM_OUT_BYTES
+column VM_OUT_BYTES  NEW_VALUE VM_OUT_BYTES noprint
+select to_char(value) "VM_OUT_BYTES" from v$osstat where STAT_NAME='VM_OUT_BYTES';
+
+
+prompt  <center> <table WIDTH=600 BORDER=1> 
+
+  prompt  <tr>
+      prompt <th> 属性 </th>
+      prompt <th> 属性值 </th>
+  prompt  </tr>
+  prompt  <tr>
+    prompt  <td> 活动CPU个数 </td>
+    prompt  <td> &CPUS  </td>
+  prompt  </tr>
+  prompt  <tr>
+    prompt  <td> 操作系统物理内存大小 </td>
+    prompt  <td> &MEMORYBYTES  </td>
+  prompt  </tr>
+  prompt  <tr>
+    prompt  <td> 由于虚拟内存交换而调入的字节数 </td>
+    prompt  <td> &VM_IN_BYTES  </td>
+  prompt  </tr>
+  prompt  <tr>
+    prompt  <td> 由于虚拟内存交换而调出的字节数</td>
+    prompt  <td> &VM_OUT_BYTES </td>
+  prompt  </tr>
+  prompt </table>  </center> <br> <br>
+
+
 
